0ddef0ff0d3a9684f394e619e79159976ee8a19b
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-06-08 15:30:09 -0700

    complete

--- src/EmployeeList.jsx
+++ src/EmployeeList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Badge, Button, Table, Card } from 'react-bootstrap'
+import { Badge, Button, Table, Card, Modal } from 'react-bootstrap'
 import { useLocation } from 'react-router-dom'
 import EmployeeFilter from './EmployeeFilter.jsx'
 import EmployeeAdd from './EmployeeAdd.jsx'
@@ -48,22 +48,57 @@ function EmployeeTable(props)  {
         </Card>
     )
 }
-
-function EmployeeRow(props) {
-    function onDeleteClick() {
-        props.deleteEmployee(props.employee._id)
+export class EmployeeRow extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            modalVisible: false,
+        }
+        this.toggleModal = this.toggleModal.bind(this)
+        this.onDeleteClick = this.onDeleteClick.bind(this)
     }
-    return (
-        <tr>
-            <td><Link to={`/edit/${props.employee._id}`}>{props.employee.name}</Link></td>
-            <td>{props.employee.extension}</td>
-            <td>{props.employee.email}</td>
-            <td>{props.employee.title}</td>
-            <td>{props.employee.dateHired.toDateString()}</td>
-            <td>{props.employee.currentlyEmployed ? 'Yes' : 'No'}</td>
-            <td><Button variant='danger' size='sm' onClick={onDeleteClick}>X</Button></td>
-        </tr>
-    )
+    onDeleteClick() {
+        this.props.deleteEmployee(this.props.employee._id)
+    }
+    toggleModal() {
+        this.setState({modalVisible: !this.state.modalVisible})
+    }
+
+    render() {
+        return (
+            <>
+                <tr>
+                    <td><Link to={`/edit/${this.props.employee._id}`}>{this.props.employee.name}</Link></td>
+                    <td>{this.props.employee.extension}</td>
+                    <td>{this.props.employee.email}</td>
+                    <td>{this.props.employee.title}</td>
+                    <td>{this.props.employee.dateHired.toDateString()}</td>
+                    <td>{this.props.employee.currentlyEmployed ? 'Yes' : 'No'}</td>
+                    <td>
+                        <Button
+                            variant='danger'
+                            size='sm'
+                            onClick={this.toggleModal}>
+                                X
+                        </Button>
+                    </td>
+                </tr>
+                <Modal show={this.state.modalVisible} onHide={this.toggleModal} centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Delete Employee?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Are you sure you want to delete this employee?
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant='danger' size='sm'className='mt-4' onClick={this.toggleModal}>Cancel</Button>
+                        <Button type='submit' variant='success' size='sm' className='mt-4' onClick={this.onDeleteClick}>Yes</Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+
+        )
+    }
 }
 export default class EmployeeList extends React.Component {
     constructor() {
@@ -115,9 +150,9 @@ export default class EmployeeList extends React.Component {
     render(){
         return(
             <React.Fragment>
-                    <EmployeeFilter/>
-                    <EmployeeTable employees={this.state.employees} deleteEmployee={this.deleteEmployee} />
-                    <EmployeeAdd createEmployee = {this.createEmployee} />
+                <EmployeeAdd createEmployee = {this.createEmployee} />
+                <EmployeeFilter/>
+                <EmployeeTable employees={this.state.employees} deleteEmployee={this.deleteEmployee} />
             </React.Fragment>
         )
     }
